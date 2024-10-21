77b73320020bf4d07f89f3b4763fba0b730d0b2d
ldcumg <wonbin145@gmail.com>
2024-10-21 10:41:46 +0900

    project: first commit

--- src/components/providers/QueryProvider.tsx
+++ src/components/providers/QueryProvider.tsx
@@ -31,10 +31,10 @@ function getQueryClient() {
 }
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
+  // Avoid useState when initializing the query client if you don't
+  // have a suspense boundary between this and the code that may
+  // suspend because React will throw away the client on the initial
+  // render if it suspends and there is no boundary
   const queryClient = getQueryClient();
 
   return (
