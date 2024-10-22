69f0c8d50331b96ad596cf78eff69ae24b1e5c44
yuna <yuna744@gmail.com>
2024-10-23 02:55:50 +0900

    feat: server client 받아오는 형식 확인

--- src/app/tourInfo/page.tsx
+++ src/app/tourInfo/page.tsx
@@ -38,98 +38,11 @@
 //   );
 // };
 // export default GroupDetailPage;
+
 'use client';
 
-import browserClient from '@/utils/supabase/client';
 import { useQuery } from '@tanstack/react-query';
-import { useState, useEffect } from 'react';
-
-// 'use client';
-// import { fetchGroupsFromApi } from '@/services/client-action/groupAction';
-// import { useQuery } from '@tanstack/react-query';
-// const GroupDetailPage = () => {
-//   const {
-//     data: groups,
-//     error,
-//     isLoading,
-//   } = useQuery({
-//     queryKey: ['groups'],
-//     queryFn: fetchGroupsFromApi,
-//   });
-//   if (isLoading) {
-//     return <p>로딩 중...</p>;
-//   }
-//   if (error instanceof Error) {
-//     return <p>에러 발생: {error.message}</p>;
-//   }
-//   return (
-//     <div>
-//       <h1 className='text-xl'>Group Details</h1>
-//       <hr />
-//       {groups && groups.length > 0 ? (
-//         groups.map((group: any) => (
-//           <div key={group.group_id}>
-//             <p>Title: {group.group_title}</p>
-//             <p>Description: {group.group_desc}</p>
-//             <p>Invite Code: {group.group_invite_code}</p>
-//             <p>Status: {group.group_status}</p>
-//             <p>Created At: {group.created_at}</p>
-//             <hr />
-//           </div>
-//         ))
-//       ) : (
-//         <p>그룹 정보를 찾을 수 없습니다.</p>
-//       )}
-//     </div>
-//   );
-// };
-// export default GroupDetailPage;
-
-// 'use client';
-
-// import { fetchGroupsFromApi } from '@/services/client-action/groupAction';
-// import { useQuery } from '@tanstack/react-query';
-
-// const GroupDetailPage = () => {
-//   const {
-//     data: groups,
-//     error,
-//     isLoading,
-//   } = useQuery({
-//     queryKey: ['groups'],
-//     queryFn: fetchGroupsFromApi,
-//   });
-
-//   if (isLoading) {
-//     return <p>로딩 중...</p>;
-//   }
-
-//   if (error instanceof Error) {
-//     return <p>에러 발생: {error.message}</p>;
-//   }
-
-//   return (
-//     <div>
-//       <h1 className='text-xl'>Group Details</h1>
-//       <hr />
-//       {groups && groups.length > 0 ? (
-//         groups.map((group: any) => (
-//           <div key={group.group_id}>
-//             <p>Title: {group.group_title}</p>
-//             <p>Description: {group.group_desc}</p>
-//             <p>Invite Code: {group.group_invite_code}</p>
-//             <p>Status: {group.group_status}</p>
-//             <p>Created At: {group.created_at}</p>
-//             <hr />
-//           </div>
-//         ))
-//       ) : (
-//         <p>그룹 정보를 찾을 수 없습니다.</p>
-//       )}
-//     </div>
-//   );
-// };
-// export default GroupDetailPage;
+import browserClient from '@/utils/supabase/client';
 
 const fetchGroups = async () => {
   const supabase = browserClient;
