76d411ab791237b8eb16750be32a82c5e0938dc1
escape-engineering <tkdrnr1215@naver.com>
2024-10-23 00:41:36 +0900

    feat: 초대 수락 관련 기능 구현

---
+++ src/app/acceptinvite/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useInsertUserGroupMutation } from '@/hooks/queries/byUse/useGroupMutations';
+import { checkMemberexist } from '@/services/client-action/groupActions';
+import { makeUserGroupDataToObj } from '@/services/groupServices';
+import browserClient from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+
+type Props = {
+  searchParams: { group_id: string };
+};
+
+const AcceptInvitePage = ({ searchParams: { group_id } }: Props) => {
+  const router = useRouter();
+  console.log('group_id :>> ', group_id);
+  if (!group_id) {
+    alert('존재하지 않는 초대링크입니다!');
+    router.push('/');
+  }
+  const { isError: insertUserGroupError, mutate: insertUserGroupMutate } = useInsertUserGroupMutation();
+  const onClick = async () => {
+    const { data } = await browserClient.auth.getUser();
+    if (data.user?.id) {
+      const userId = data.user.id;
+      //NOTE - 유저가 이미 해당 그룹에 속해있는지 검사 / 체크도 tanstack query로 관리해야하나? 의미가 있을지 의문
+      const { data: existingMember, error: existingMemberError } = await checkMemberexist(group_id, userId);
+      if (existingMemberError) throw new Error('');
+      const isExisting = !!existingMember?.length;
+      if (isExisting) {
+        alert('이미 속해있는 그룹입니다.');
+        return;
+      }
+      const userGroupObj = makeUserGroupDataToObj(userId, false, group_id);
+      insertUserGroupMutate(userGroupObj);
+    }
+  };
+
+  return (
+    <div>
+      <p></p>
+      <button onClick={onClick}>수락</button>
+      <button onClick={() => router.push('/')}>거절</button>
+    </div>
+  );
+};
+
+export default AcceptInvitePage;
--- src/hooks/queries/byUse/useGroupMutations.ts
+++ src/hooks/queries/byUse/useGroupMutations.ts
@@ -3,6 +3,13 @@ import { GroupObjType, UserGroupType } from '@/types/groupTypes';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
+type ErrorTypes = {
+  code: string | null;
+  detail: string | null;
+  hint: string | null;
+  message: string | null;
+};
+
 // TODO - invalidate필요시 쿼리키 추가 필요
 const useInsertGroupMutation = () => {
   return useMutation({
@@ -21,7 +28,11 @@ const useInsertUserGroupMutation = () => {
     onSuccess: (data) => {
       router.push(`/groupdetail?group_id=${data?.groupId}`);
     },
-    onError: (error) => console.log('error :>> ', error),
+    onError: (error: ErrorTypes) => {
+      if (error.code === '22P02' || error.code === '23503')
+        alert('해당 그룹에 입장할 수 없습니다. 초대코드를 다시 확인해주세요.');
+      console.log('error :>> ', error);
+    },
   });
 };
 
