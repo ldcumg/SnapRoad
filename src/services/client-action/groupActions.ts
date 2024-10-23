0a0906d194fb894284139d12e4aca2f06781b558
escape-engineering <tkdrnr1215@naver.com>
2024-10-23 21:17:37 +0900

    feat: 그룹 이미지 업로드 로직 분리

--- src/services/client-action/groupActions.ts
+++ src/services/client-action/groupActions.ts
@@ -1,19 +1,27 @@
 import { GroupObjType, UserGroupType } from '@/types/groupTypes';
 import browserClient from '@/utils/supabase/client';
 
+const uploadDefaultGroupImg = async (group_id: string) => {
+  const defaultImgRes = await fetch('/images/group_default_thumbnail.png');
+  const defaultImgBlob = await defaultImgRes.blob();
+  const { data, error } = await browserClient.storage
+    .from('group_image')
+    .upload(`${group_id}_thumbnail`, defaultImgBlob);
+  return { data, error };
+};
+
+const uploadGroupImg = async (groupImg: File, group_id: string) => {
+  const { data, error } = await browserClient.storage.from('group_image').upload(`${group_id}_thumbnail`, groupImg);
+  return { data, error };
+};
+
 const insertGroupData = async (groupObj: GroupObjType, groupImg: File | null) => {
   //NOTE - 그룹 이미지가 없을 시 public의 기본썸네일 사용, 있을 시 업로드한 이미지 사용
   if (!groupImg) {
-    const defaultImgRes = await fetch('/images/group_default_thumbnail.png');
-    const defaultImgBlob = await defaultImgRes.blob();
-    const { data, error } = await browserClient.storage
-      .from('group_image')
-      .upload(`${groupObj.group_id}_thumbnail`, defaultImgBlob);
+    const { data, error } = await uploadDefaultGroupImg(groupObj.group_id);
     if (error) throw error;
   } else {
-    const { data, error } = await browserClient.storage
-      .from('group_image')
-      .upload(`${groupObj.group_id}_thumbnail`, groupImg);
+    const { data, error } = await uploadGroupImg(groupImg, groupObj.group_id);
     if (error) throw error;
   }
 
