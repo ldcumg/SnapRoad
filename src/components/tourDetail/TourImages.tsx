0405670d5bcd2b5c1e472ffb285ac241bf7b28f1
jungminji <wjdalswl0215@gmail.com>
2024-10-29 04:51:45 +0900

    feat : 캐러셀 touch event

--- src/components/tourDetail/TourImages.tsx
+++ src/components/tourDetail/TourImages.tsx
@@ -11,6 +11,9 @@ const TourImages = ({ images }: TourImagesProps) => {
   // 현재 선택한 사진의 인덱스를 확인하는 상태변수
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const [touchX, setTouchX] = useState(0);
+  const [touchY, setTouchY] = useState(0);
+
   const moveCarousel = useRef<HTMLInputElement>(null);
 
   /** 이전 */
@@ -27,17 +30,40 @@ const TourImages = ({ images }: TourImagesProps) => {
     }
   };
 
+  const onTouchStart = (e: React.TouchEvent) => {
+    setTouchX(e.changedTouches[0].pageX);
+    setTouchY(e.changedTouches[0].pageY);
+  };
+
+  const onTouchEnd = (e: React.TouchEvent) => {
+    const distanceX = touchX - e.changedTouches[0].pageX;
+    const distanceY = touchY - e.changedTouches[0].pageY;
+    const vector = Math.abs(distanceX / distanceY);
+
+    if (distanceX > 30 && vector > 2) {
+      handleMoveNext();
+    } else if (distanceX < -30 && vector > 2) {
+      handleMovePrev();
+    }
+  };
+
   // useEffect 훅을 사용하여 currentIndex가 변경될 때만 실행되도록 설정
   useEffect(() => {
     if (moveCarousel.current) {
       // moveCarousel.current.style.transform = `translateX(-${currentIndex * 412}px)`;
+      moveCarousel.current.style.transition = 'transform 0.5s ease'; // 부드러운 이동을 위한 트랜지션 설정
+
       moveCarousel.current.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
   }, [currentIndex]); // currentIndex가 변경될 때마다 실행
 
   return (
     <>
-      <div className='flex w-[412px] h-[300px] justify-start items-center overflow-hidden'>
+      <div
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
+        className='flex w-[412px] h-[300px] justify-start items-center overflow-hidden'
+      >
         <div
           ref={moveCarousel}
           className='flex w-full	h-full'
