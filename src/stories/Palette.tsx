5f7bdd6109e68d0827d4ff93b0180cb6d8b90c0b
yuna <yuna744@gmail.com>
2024-11-02 15:07:56 +0900

    feat: palette storybook 추가

--- src/stories/Palette.tsx
+++ src/stories/Palette.tsx
@@ -35,7 +35,7 @@ export const Palette = () => {
   ];
 
   const handleCopy = (className: string) => {
-    const colorName = className.replace(/^bg-/, ''); // 'bg-' 제거
+    const colorName = className.replace(/^bg-/, '');
     navigator.clipboard.writeText(colorName).then(() => {
       setCopiedColor(colorName);
       setTimeout(() => setCopiedColor(null), 2000);
@@ -45,7 +45,7 @@ export const Palette = () => {
   return (
     <div className='p-8'>
       <h1 className='text-2xl font-bold mb-8'>Color</h1>
-      <div className='grid grid-cols-3 gap-4'>
+      <div className='grid grid-cols-3 md:grid-cols-6 gap-4'>
         {colorsData.map((color) => (
           <div
             key={color.label}
