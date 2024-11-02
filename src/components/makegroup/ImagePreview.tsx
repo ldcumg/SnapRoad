27617e0a1f069b4c55eb532a9161809deb23b051
escape-engineering <tkdrnr1215@naver.com>
2024-11-03 01:23:40 +0900

    style: 그룹 생성/업데이트 페이지 이미지 관련 스타일작업

--- src/components/makegroup/ImagePreview.tsx
+++ src/components/makegroup/ImagePreview.tsx
@@ -6,30 +6,32 @@ const ImagePreview = ({ imgPreview }: Props) => {
   return (
     <>
       {imgPreview ? (
-        <div className='relative flex justify-center items-center w-[182px] h-[182px]'>
+        <div className='relative flex justify-center items-start w-[232px] h-[216px] rounded-xl'>
           <img
             src={imgPreview}
             alt='업로드 그룹 썸네일 이미지'
-            className='flex justify-center items-center w-[130px] h-[130px] border-[2px] border-black border-solid'
+            className='flex justify-center items-center w-[200px] h-[200px] border border-gray-100 rounded-xl'
           />
-          <div className='absolute bottom-0 right-0 w-[52px] h-[52px] flex justify-center items-center border border-solid border-black rounded-[26px] bg-white'>
+          <div className='absolute bottom-0 right-0 w-[48px] h-[48px] flex justify-center items-center border border-solid border-black rounded-[24px] bg-white'>
             <img
               src='/svgs/cameraIcon.svg'
               alt=''
+              className='w-6 h-6'
             />
           </div>
         </div>
       ) : (
-        <div className='relative flex justify-center items-center w-[182px] h-[182px]'>
+        <div className='relative flex justify-center items-start rounded-xl w-[232px] h-[216px]'>
           <img
-            className='w-[130px] h-[130px]'
+            className='w-[200px] h-[200px] rounded-xl'
             src='/images/group_default_thumbnail.png'
             alt=''
           />
-          <div className='absolute bottom-0 right-0 w-[52px] h-[52px] flex justify-center items-center border border-solid border-black rounded-[26px] bg-white'>
+          <div className='absolute bottom-0 right-0 w-[48px] h-[48px] flex justify-center items-center border border-solid border-gray-100 shadow-BG_S rounded-[24px] bg-white'>
             <img
               src='/svgs/cameraIcon.svg'
               alt=''
+              className='w-6 h-6'
             />
           </div>
         </div>
