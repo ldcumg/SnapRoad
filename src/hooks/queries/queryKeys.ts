eb0e077d5376fd0d4c9f714cf4f752530f2924a4
escape-engineering <tkdrnr1215@naver.com>
2024-11-05 00:47:31 +0900

    refactor: queryKey 분리

--- src/hooks/queries/queryKeys.ts
+++ src/hooks/queries/queryKeys.ts
@@ -3,6 +3,10 @@ const queryKeys = {
     info: (groupId: string) => [groupId, 'groupInfo'],
     posts: (groupId: string) => [groupId, 'groupPosts'],
     postsImages: (groupId: string) => [groupId, 'groupPosts', 'images'],
+    groupForUpdate: (groupId: string) => [groupId, 'groupDetailForUpdate'],
+    groupDetail: (groupId: string) => [groupId, 'groupDetail'],
+    groupList: () => ['groupList'],
+    groupRandomPosts: () => ['groupRandomPosts'],
   },
 };
 
