b58903f8100cd9fe7cc78ee20840a3ec4a68660f
escape-engineering <tkdrnr1215@naver.com>
2024-11-05 19:46:16 +0900

    feat: 로그인 후 랜딩페이지 접근 시 그룹 리스트 페이지로 이동하게끔 변경

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -29,6 +29,12 @@ export const updateSession = async (request: NextRequest) => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  if (user && request.nextUrl.pathname === '/') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/grouplist';
+    return NextResponse.redirect(url);
+  }
+
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
