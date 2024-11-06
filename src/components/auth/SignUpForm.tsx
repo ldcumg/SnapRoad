311adbbb688652bd72aac7b77b53cb2a7549f178
jungminji <wjdalswl0215@gmail.com>
2024-11-06 20:58:32 +0900

    feat : 약관 모달

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import AgreeCheck from './AgreeList';
+import AgreeList from './AgreeList';
 import { useSignUpForm } from '@/hooks/byUse/useAuthForm';
 import { useSignUp } from '@/hooks/queries/byUse/useAuthMutations';
 import { signUpSchema } from '@/schemas/authSchemas';
 import useBottomSheetStore from '@/stores/story/useBottomSheetStore';
-import { BottomSheet } from '@/stories/BottomSheet';
+import { AgreeBottomSheet } from '@/stories/AgreeBottomSheet';
+// import { BottomSheet } from '@/stories/BottomSheet';
 import { Button } from '@/stories/Button';
 import { Input } from '@/stories/Input';
 import Link from 'next/link';
@@ -115,7 +118,7 @@ const SignUpForm = () => {
       </div>
 
       {/* 모달 UI */}
-      <BottomSheet
+      <AgreeBottomSheet
         isOpen={isFullHeightOpen}
         onClose={handleFullClose}
         title='개인정보 수집·이용 약관 동의'
@@ -130,43 +133,9 @@ const SignUpForm = () => {
               className='w-[24px] h-[24px]'
               onClick={handleCheckboxToggle}
             />
-
-            <div>
-              <span className='text-gray-900 text-label_md'>[필수] 개인정보 수집 및 이용에 대한 동의</span>
-              <div>
-                <span className='text-gray-900 text-body_sm'>
-                  1. 수집하는 개인정보 항목회원가입 및 서비스 이용을 위해 다음과 같은 개인정보를 수집합니다.
-                </span>
-                <span className='text-gray-700 text-label_sm'>- 필수 정보 : 이메일 주소, 비밀번호</span>
-              </div>
-
-              <div>
-                <span className='text-gray-900 text-body_sm'>2. 개인정보 수집 및 이용 목적</span>
-                <span className='text-gray-900 text-body_sm'>수집한 개인정보는 다음 목적을 위해 사용됩니다</span>
-                <span className='text-gray-700 text-label_sm'>- 회원가입 의사 확인 및 회원 식별</span>
-              </div>
-
-              <div>
-                <span className='text-gray-900 text-body_sm'>3. 개인정보 보유 및 이용 기간</span>
-                <span className='text-gray-900 text-body_sm'>
-                  원칙적으로, 개인정보는 수집 및 이용 목적이 달성된 후에는 해당 정보를 지체 없이 파기합니다. 다만,
-                  아래의 정보는 명시한 기간 동안 보관됩니다.
-                </span>
-
-                <span className='text-gray-700 text-label_sm'>- 회원가입 및 관리 : 회원 탈퇴 시까지 보유</span>
-              </div>
-
-              <div>
-                <span className='text-gray-900 text-body_sm'>4. 개인정보의 파기 절차 및 방법</span>
-                <span className='text-gray-900 text-body_sm'>
-                  원칙적으로, 개인정보 수집 및 이용 목적이 달성된 후에는 해당 정보를 지체 없이 파기합니다. 전자적 파일
-                  형태의 정보는 기록을 재생할 수 없느니술적 방법을 사용하여 삭제합니다. 종이에 출력된 정보는 분쇄기로
-                  분쇄하거나 속하여 파기합니다.
-                </span>
-              </div>
-            </div>
+            <AgreeList />
           </div>
-          <div className='flex py-5 px-4 justify-center gap-2'>
+          <div className='flex px-4 justify-center gap-2'>
             <Button
               label='취소'
               onClick={handleFullClose}
@@ -178,7 +147,7 @@ const SignUpForm = () => {
             />
           </div>
         </div>
-      </BottomSheet>
+      </AgreeBottomSheet>
     </div>
   );
 };
---
+++ src/stories/AgreeBottomSheet.tsx
@@ -0,0 +1,89 @@
+import { cn } from '@/lib/utils';
+import { ReactNode, HTMLAttributes, useEffect, useState } from 'react';
+
+interface BottomSheetProps extends HTMLAttributes<HTMLDivElement> {
+  isOpen: boolean;
+  onClose: () => void;
+  onBack?: () => void;
+  title: string;
+  buttonLabel?: string;
+  onButtonClick: () => void;
+  children: ReactNode;
+  height?: 'full' | 'half';
+}
+
+export const AgreeBottomSheet = ({
+  isOpen,
+  onClose,
+  onBack,
+  title,
+  buttonLabel,
+  onButtonClick,
+  children,
+  height = 'half',
+  ...props
+}: BottomSheetProps) => {
+  const [rendered, setRendered] = useState(isOpen);
+
+  useEffect(() => {
+    if (isOpen) {
+      setRendered(true);
+    } else {
+      const timer = setTimeout(() => setRendered(false), 500);
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen]);
+
+  const baseStyle =
+    'fixed bottom-0 left-0 right-0 bg-white transition-transform transition-opacity duration-500 ease-in-out';
+  const heightStyle = height === 'full' ? 'h-full' : 'h-1/2';
+  const visibilityStyle = isOpen ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0';
+
+  return (
+    <>
+      {/* 배경 */}
+      {rendered && (
+        <div
+          className={`fixed inset-0 bg-black transition-opacity duration-500 ease-in-out ${
+            isOpen ? 'opacity-50' : 'opacity-0 hidden'
+          }`}
+          onClick={onClose}
+        ></div>
+      )}
+
+      {/* 바텀시트 */}
+      {rendered && (
+        <div
+          className={cn(baseStyle, heightStyle, visibilityStyle, 'pb-16 z-50')}
+          {...props}
+        >
+          <div className='w-12 h-1 bg-gray-300 mx-auto my-2'></div>
+
+          <div className='flex items-center justify-between px-4 py-2 border-b border-gray-300'>
+            {onBack && (
+              <button
+                onClick={onBack}
+                aria-label='Back'
+                className='text-gray-700'
+              >
+                ← 뒤로가기
+              </button>
+            )}
+
+            <h2 className='text-center font-semibold flex-1'>{title}</h2>
+
+            <button
+              onClick={onClose}
+              aria-label='Close'
+              className='text-gray-700'
+            >
+              × 닫기
+            </button>
+          </div>
+
+          <div className='p-4 text-gray-700'>{children}</div>
+        </div>
+      )}
+    </>
+  );
+};
