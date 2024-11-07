2d3fc8739883349d036d509fa1fca0fa53a7df05
escape-engineering <tkdrnr1215@naver.com>
2024-11-07 22:18:12 +0900

    style: 그룹리스트페이지 로딩 시 스피너 화면에 보이도록 추가

--- src/components/makegroup/MakeGroupForm.tsx
+++ src/components/makegroup/MakeGroupForm.tsx
@@ -22,12 +22,30 @@ const MakeGroupForm = ({ update_for }: Props) => {
   const { register, handleSubmit, formState, watch, reset, setValue, clearErrors } = useMakeGroupForm();
   const [imgPreview, setImgPreview] = useState<string | null>(null);
 
-  const { isError: insertGroupDataError, mutateAsync: insertGroupDataMutate } = useInsertGroupMutation();
-  const { isError: insertUserGroupError, mutate: insertUserGroupMutate } = useInsertUserGroupMutation();
+  //NOTE - 업데이트 상태일 시 데이터 가져오기
+  const { data: groupDetailData, isPending: isPendingBeforeData } = useGroupDetailQueryForUpdate(update_for as string);
+  //NOTE - 그룹 테이블 insert mutation
+  const {
+    isError: insertGroupDataError,
+    mutateAsync: insertGroupDataMutate,
+    isPending: isPendingInsertGroup,
+  } = useInsertGroupMutation();
+  //NOTE - 유저_그룹테이블 insert mutation
+  const {
+    isError: insertUserGroupError,
+    mutate: insertUserGroupMutate,
+    isPending: isPendingInsertUserGroup,
+  } = useInsertUserGroupMutation();
+  //NOTE - 그룹 테이블 update mutation
+  const {
+    isError: updateGroupDataError,
+    mutateAsync: updateGroupDataMutate,
+    isPending: isPendingUpdate,
+  } = useUpdateGroupMutation(update_for as string);
 
-  const { isError: updateGroupDataError, mutateAsync: updateGroupDataMutate } = useUpdateGroupMutation(
-    update_for as string,
-  );
+  const isFetchingBeforeData = isPendingBeforeData && update_for;
+  const isInserting = isPendingInsertGroup || isPendingInsertUserGroup;
+  const isUpdating = isPendingUpdate && update_for;
 
   const onSubmit = async (value: FieldValues) => {
     //TODO - 파일 확장자 관련 유효성 검사 필요
@@ -51,14 +69,11 @@ const MakeGroupForm = ({ update_for }: Props) => {
     const response = await fetch(url);
     const blob = await response.blob();
     const file = new File([blob], 'group_image.jpg', { type: blob.type });
-
     // FileList 객체 생성
     const dataTransfer = new DataTransfer();
     dataTransfer.items.add(file);
     return dataTransfer.files;
   };
-
-  const { data: groupDetailData, isPending, isLoading } = useGroupDetailQueryForUpdate(update_for as string);
 
   useEffect(() => {
     const fillGroupData = async () => {
@@ -96,14 +111,14 @@ const MakeGroupForm = ({ update_for }: Props) => {
   if (insertGroupDataError || insertUserGroupError || updateGroupDataError) throw new Error('에러 발생!');
   return (
     <>
-      {update_for && isPending && (
-        <div className='w-full h-full absolute z-[3000] bg-black opacity-10 flex justify-center items-center'>
-          <Spinner />
+      {(isFetchingBeforeData || isInserting || isUpdating) && (
+        <div className='absolute z-[3000] flex h-full w-full items-center justify-center bg-black bg-opacity-10'>
+          <Spinner color='primary-400' />
         </div>
       )}
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='flex flex-col justify-center items-center gap-6 px-4 pt-4'
+        className='flex flex-col items-center justify-center gap-6 px-4 pt-4'
       >
         <InputSection
           register={register}
@@ -114,7 +129,7 @@ const MakeGroupForm = ({ update_for }: Props) => {
           clearInputValue={clearInputValue}
         />
         <button
-          className='cursor-pointer bg-primary-400 w-full py-4 rounded-xl text-white text-title_lg'
+          className='w-full cursor-pointer rounded-xl bg-primary-400 py-4 text-title_lg text-white'
           type='submit'
         >
           {update_for ? '수정 완료' : '그룹 만들기'}
