1507259fd00b9298059ff236dc51c195ee914b89
escape-engineering <tkdrnr1215@naver.com>
2024-11-08 01:07:53 +0900

    feat: loading 페이지 생성

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+import DotSpinner from '@/components/_common/DotSpinner';
+
+const Loading = () => {
+  return (
+    <div className='flex h-screen w-full items-center justify-center'>
+      <section className='flex flex-col items-center gap-[23px]'>
+        <DotSpinner />
+        <p className='text-label_md text-gray-700'>잠시만 기다려주세요...</p>
+      </section>
+    </div>
+  );
+};
+
+export default Loading;
