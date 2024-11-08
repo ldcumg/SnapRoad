86fdfe0938a798a5ba6cdc8ff56b03b90fdba046
yuna <yuna744@gmail.com>
2024-11-08 17:17:47 +0900

    feat: textarea storybook 공통 컴포넌트화

--- src/components/post/FixPostForm.tsx
+++ src/components/post/FixPostForm.tsx
@@ -1,6 +1,9 @@
+import { formSchema } from './query/formSchemas';
 import { useForm } from './query/useFormMutations';
 import { usePostForm } from './query/usePostForm';
 import { Button } from '@/stories/Button';
+import TextAreaWithCounter from '@/stories/TextAreas';
+import { useMemo, useState } from 'react';
 import { FieldValues } from 'react-hook-form';
 
 const FixPostForm = () => {
@@ -12,22 +15,46 @@ const FixPostForm = () => {
   } = usePostForm();
 
   const { mutate: postForm } = useForm();
+  const [isChecked, setIsChecked] = useState(false);
+  const [isCheckedInSheet, setIsCheckedInSheet] = useState(false);
 
-  const onSubmit = async (value: FieldValues) => {
+  const handlePostForm = async (value: FieldValues) => {
     console.log('폼 데이터 제출', value);
+    postForm(formSchema.parse(value));
   };
+
+  // 필드 값 감시
+  const text = watch('text');
+  const hashtags = watch('hashtags');
+  const date = watch('date');
+  const time = watch('time');
+
+  // 필드 빈값 확인
+  const isFormValueFilled = useMemo(() => {
+    return text && hashtags && date && time;
+  }, [text, hashtags, date, time]);
 
   return (
     <>
       <form
         className='flex flex-col'
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(handlePostForm)}
       >
-        <textarea
+        <TextAreaWithCounter
+          id='formValue'
           {...register('text')}
+          placeholder='내용 입력항목입니다.'
+          maxLength={1000}
+          errorText={errors.text && String(errors.text.message)}
+          variant='default'
+        />
+
+        {/* <textarea
+          {...register('text')}
+          maxLength={1000}
           placeholder='내용 입력'
         />
-        {errors.text && <span>{errors.text.message && String(errors.text.message)}</span>}
+        {errors.text && <span>{errors.text.message && String(errors.text.message)}</span>} */}
 
         <input
           {...register('hashtags')}
@@ -47,7 +74,12 @@ const FixPostForm = () => {
         />
         {errors.time && <span>{errors.time.message && String(errors.time.message)}</span>}
 
-        <button type='submit'>업로드</button>
+        <Button
+          type='submit'
+          label='게시물 업로드'
+          variant='primary'
+          // disabled={!isFormValueFilled || Object.keys(errors).length > 0}
+        />
       </form>
     </>
   );
