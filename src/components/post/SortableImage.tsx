94020678301c400391580a4a7e1818ac3ffe2a61
yuna <yuna744@gmail.com>
2024-11-11 08:01:26 +0900

    fix: slide 누락 추가

--- src/components/post/SortableImage.tsx
+++ src/components/post/SortableImage.tsx
@@ -15,6 +15,9 @@ const SortableImage = ({ image, onSetCover, selectedCover }: SortableImageProps)
     transition,
     border: selectedCover === image.id ? '2px solid #4CAF50' : '2px solid transparent', // 대표 이미지에 테두리 색상 적용
     boxShadow: selectedCover === image.id ? '0 4px 8px rgba(76, 175, 80, 0.6)' : 'none', // 대표 이미지에 그림자 추가
+    minWidth: '240px',
+    maxWidth: '240px',
+    height: '240px',
   };
 
   return (
@@ -23,26 +26,26 @@ const SortableImage = ({ image, onSetCover, selectedCover }: SortableImageProps)
       style={style}
       {...attributes}
       {...listeners}
-      className='keen-slider__slide min-w-[240px] max-w-[240px] h-[240px] cursor-pointer flex flex-col items-center relative'
+      className='keen-slider__slide relative flex cursor-pointer flex-col items-center'
       onClick={() => image.id && onSetCover(image.id)}
     >
       <div className='absolute bottom-2 right-2 z-10'>
         {selectedCover === image.id ? (
-          <span className='bg-primary-400 rounded-xl text-xs text-white px-2 py-1'>대표</span>
+          <span className='rounded-xl bg-primary-400 px-2 py-1 text-xs text-white'>대표</span>
         ) : (
           <span
             style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
-            className='rounded-xl text-xs text-white px-2 py-1'
+            className='rounded-xl px-2 py-1 text-xs text-white'
           >
             대표
           </span>
         )}
       </div>
-      <div className='h-full w-full'>
+      <div>
         <img
           src={image.blobUrl}
           alt='업로드 이미지'
-          className='w-full h-full object-cover'
+          className='h-full w-full object-cover'
         />
       </div>
     </div>
