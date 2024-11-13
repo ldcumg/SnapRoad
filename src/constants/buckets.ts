0da603a30466ce6f295ff4460aa9f7eaf6fbb538
ldcumg <ldcumg@naver.com>
2024-11-13 11:41:29 +0900

    Update buckets.ts

--- src/constants/buckets.ts
+++ src/constants/buckets.ts
@@ -2,7 +2,7 @@ enum buckets {
   groupImage = 'group_image',
   avatars = 'avatars',
   tourImages = 'tour_images',
-  public_images = 'public_images',
+  publicImages = 'public_images',
 }
 
 export default buckets;
