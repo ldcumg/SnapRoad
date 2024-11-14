0e77b287278c5ca588c28df1681b0791992e049c
escape-engineering <tkdrnr1215@naver.com>
2024-11-14 17:13:30 +0900

    refactor: prefetch 적용, 그룹리스트가 없으면 randompost안나타나도록

--- src/components/groupList/RandomImage.tsx
+++ src/components/groupList/RandomImage.tsx
@@ -4,23 +4,31 @@ import { Card, CardContent } from '../ui/card';
 import RandomImageSkeleton from './RandomImageSkeleton';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 import { useGroupRandomImageQuery } from '@/hooks/queries/group/useGroupQueries';
+import queryKeys from '@/hooks/queries/queryKeys';
+import { GroupWithCounts } from '@/types/groupTypes';
 import { formatDateToYY_MM_DD } from '@/utils/dateUtils';
 import { getSlicedAddress } from '@/utils/getSlicedAddress';
+import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 import Autoplay from 'embla-carousel-autoplay';
 import Link from 'next/link';
 
 const RandomImage = () => {
   const { data: randomData, isLoading } = useGroupRandomImageQuery();
   if (isLoading) return <RandomImageSkeleton />;
-
   const items = randomData ?? [];
+
+  //NOTE - 소속 그룹이 없을 때 게시글 작성이 아닌 그룹 생성을 유도하기 위해 필요한 데이터
+  const queryClient = useQueryClient();
+  const groupListData: InfiniteData<GroupWithCounts[]> | undefined = queryClient.getQueryData(
+    queryKeys.group.groupList(),
+  );
+  const groupListDataLen = groupListData?.pages[0].length;
 
   const carouselOptions = {
     loop: items.length > 2,
     plugins: items.length > 2 ? [Autoplay({ delay: 3000 })] : [],
   };
   const itemClassName = items.length === 1 ? 'justify-center' : 'gap-4 pl-4';
-
   return (
     <>
       {items.length > 0 ? (
@@ -54,7 +62,7 @@ const RandomImage = () => {
             ))}
           </CarouselContent>
         </Carousel>
-      ) : (
+      ) : !!groupListDataLen ? (
         <div className='flex h-[220px] w-[220px] flex-col items-center justify-center gap-9 pt-12'>
           <img
             src='/svgs/Empty_Marker.svg'
@@ -65,6 +73,8 @@ const RandomImage = () => {
             <span className='text-center'>공유해보세요!</span>
           </p>
         </div>
+      ) : (
+        <></>
       )}
     </>
   );
