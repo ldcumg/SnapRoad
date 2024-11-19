1bcace066b4033ee7a6afd2f679bd5a95d17782b
escape-engineering <tkdrnr1215@naver.com>
2024-11-19 10:20:47 +0900

    refactor: commentForm 위치변경

--- src/components/post/postDetail/CommentUpdateForm.tsx
+++ src/components/post/postDetail/CommentUpdateForm.tsx
@@ -1,5 +1,5 @@
-import { useCommentForm } from '@/hooks/byUse/useCommentForm';
 import { useUpdateComment } from '@/hooks/queries/byUse/useCommentMutation';
+import { useCommentForm } from '@/hooks/useCustomForm/useCommentForm';
 import { Button } from '@/stories/Button';
 import { Comment, UserDetail } from '@/types/postDetailTypes';
 import React, { useEffect, useRef, useState } from 'react';
